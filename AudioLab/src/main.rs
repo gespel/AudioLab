61d5c5d2723f2531a306f69d5b81ba628b2d22d6
Sten Heimbrodt <39519505+gespel@users.noreply.github.com>
2022-10-10 04:58:12 +0200

    asd

--- AudioLab/src/main.rs
+++ AudioLab/src/main.rs
@@ -8,71 +8,31 @@ use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 
 
 struct MainHandle {
-    pub synth1: synths::SineSynth
 }
 impl MainHandle {
     
-    pub fn init(&mut self, mut r: Rack) {
+    pub fn init(&mut self) {
         let host = cpal::default_host();
         let device = host.default_output_device().expect("no output device available");
         let config = device.default_output_config().unwrap();
         match config.sample_format() {
             cpal::SampleFormat::F32 => {
-                self.run::<f32>(&device, &config.into(), r).unwrap();
+                self.run::<f32>(&device, &config.into()).unwrap();
             }
     
             cpal::SampleFormat::I16 => {
-                self.run::<i16>(&device, &config.into(), r).unwrap();
+                self.run::<i16>(&device, &config.into()).unwrap();
             }
                 
             cpal::SampleFormat::U16 => {
-                self.run::<u16>(&device, &config.into(), r).unwrap();
+                self.run::<u16>(&device, &config.into()).unwrap();
             }    
         }
 
     }
-    fn run<T>(&mut self, device: &cpal::Device, config: &cpal::StreamConfig, mut r: Rack) -> Result<(), anyhow::Error> where T: cpal::Sample {
-
-        // Get the sample rate and channels number from the config
-        let sample_rate = config.sample_rate.0 as f32;
-        let channels = config.channels as usize;
-
-        let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
-        
-        
-
-        //let mut synth1 = synths::SineSynth::new(880.0, sample_rate);
-        let mut synthy: synths::SineSynth = synths::SineSynth::new(420.0, sample_rate);
-        //let mut synthx = synths::SquareSynth::new(420.0, sample_rate);
-        //let mut synth2 = synths::SineSynth::new(5.0, sample_rate);
-        //let mut synth3 = synths::SquareSynth::new(3.0, sample_rate);
-        //let mut synth4 = synths::SquareSynth::new(0.5, sample_rate);
-        
-        // Build an output stream
-        let stream = device.build_output_stream(
-            config,
-            move |data: &mut [T], _: &cpal::OutputCallbackInfo| {
-                for frame in data.chunks_mut(channels) {
-                    //let x = synthy.get_sample()*3.0;
-                    //let x = 0.0;
-                    let mut x = r.get_sample();
-                    //let x = synthy.get_sample();
-                    //synth1.set_frequency(synth2.get_sample()*2000.0);
-                    //synth2.set_frequency(synth3.get_sample()*1000.0);
-                    //synth3.set_frequency(synth4.get_sample()*4000.0);
-                    let value: T = cpal::Sample::from::<f32>(&x);
-                    for sample in frame.iter_mut() {
-                        *sample = value;
-                    }
-                }
-            },
-            err_fn,
-        )?;
-
-        // Play the stream
-        stream.play()?;
-        
-        // Park the thread so our noise plays continuously until the app is closed
+    fn run<T>(&mut self, device: &cpal::Device, config: &cpal::StreamConfig) -> Result<(), anyhow::Error> {
+        let mut r: Rack = Rack::new(device, config);
+        r.process();
         loop {
 
         }
@@ -81,10 +41,6 @@ impl MainHandle {
 
 fn main() {
     let mut x = MainHandle {
-        synth1: synths::SineSynth::new(440.0, 48000.0)
     };
-    let mut r = racks::Rack::new();
-    let mut synth_a: synths::SineSynth = synths::SineSynth::new(420.0, 48000.0);
-    r.add_synth(synths::Synth::Sine(synth_a));
-    x.init(r);
+    x.init();
 }
--- AudioLab/src/racks.rs
+++ AudioLab/src/racks.rs
@@ -1,26 +1,53 @@
+use cpal::Stream;
+use cpal::traits::{DeviceTrait, StreamTrait};
 use crate::synths::SineSynth;
 use crate::synths::SquareSynth;
 use crate::synths::Synth;
 
 use frunk::hlist;
+use frunk::labelled::chars::T;
+use crate::synths;
 
-pub struct Rack {
-    modules: Vec<Synth>
+pub struct Rack<'a> {
+    modules: Vec<Synth>,
+    device: &'a cpal::Device,
+    config: &'a cpal::StreamConfig
 }
-impl Rack {
-    pub fn new() -> Rack {
+impl Rack<'_> {
+    pub fn new<'a>(device: &'a cpal::Device, config: &'a cpal::StreamConfig) -> Rack<'a> {
         Rack{
-            modules: Vec::new()   
+            modules: Vec::new(),
+            device: device,
+            config: config
         }
     }
     pub fn add_synth(&mut self, input: Synth) {
         self.modules.push(input);
     }
-    pub fn get_sample(&mut self) -> f32 {
-        let mut out: f32 = 0.0;
-        for module in self.modules.iter() {
-            out += module.get_sample();
-        }
-        return out;
+    pub fn process(&mut self) {
+        let sample_rate = self.config.sample_rate.0 as f32;
+        let channels = self.config.channels as usize;
+        let err_fn = |err| eprintln!("an error occurred on stream: {}", err);
+        let mut synthy: synths::SineSynth = synths::SineSynth::new(440.0, sample_rate);
+
+        let stream = self.device.build_output_stream(
+            self.config,
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                for frame in data.chunks_mut(channels) {
+                    //let x = synthy.get_sample()*3.0;
+                    //let x = 0.0;
+                    let x = synthy.get_sample();
+                    //synth1.set_frequency(synth2.get_sample()*2000.0);
+                    //synth2.set_frequency(synth3.get_sample()*1000.0);
+                    //synth3.set_frequency(synth4.get_sample()*4000.0);
+                    let value: f32 = cpal::Sample::from::<f32>(&x);
+                    for sample in frame.iter_mut() {
+                        *sample += value;
+                    }
+                }
+            },
+            err_fn,
+        );
+        stream.expect("REASON").play();
     }
 }
